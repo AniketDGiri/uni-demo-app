232d0fe2d969dad6512741e2232ab49ffc58c7f9
AnkDGiri777 <aniketgiri770@gmail.com>
2023-08-16 17:17:55 +0530

    fix: Removed unnecessary code

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Layout/Header";
-import BackgroundAnimation from "./animation/particles";
 
 const RootLayout = () => {
   return (
     <>
+      <Header />
       <Header />
       <Outlet />
     </>
